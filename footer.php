804232153b2f0db8ffd256d4b704614ca61b9a70
Bocquel <bocquel.valentin@gmail.com>
2017-12-07 20:34:01 +0100

    Add header and footer

---
+++ footer.php
@@ -0,0 +1,17 @@
+<!-- Pied de page -->
+<footer>
+	<div class="row">
+		<div class="col-md-4">
+			Telephone
+		</div>
+		<div class="col-md-4">
+			Police
+		</div>
+		<div class="col-md-4">
+			Mail
+		</div>
+	</div>
+</footer>
+
+</body>
+</html>	
---
+++ page.php
@@ -0,0 +1,9 @@
+<?php
+	include 'header.php';
+?>
+
+<!-- contenu du site -->
+
+<?php
+	include 'footer.php';
+?>
