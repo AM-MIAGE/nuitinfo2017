9bb4974aa56ff327d6165e32cf946b0671edcea0
Bocquel <bocquel.valentin@gmail.com>
2017-12-08 04:57:31 +0100

    Ajout script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('#aide').click(function(){
+		$('#aide-apparition').show();
+    });
+    $('#aide-apparition').click(function(){
+		$('#aide-apparition').hide();
+    });
+});
